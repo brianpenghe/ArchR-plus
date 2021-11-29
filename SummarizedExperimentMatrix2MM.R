9fc70d6931f27be61063d09079eee00dcf6d869a
Ubuntu <brianpenghe@gmail.com>
2021-11-29 16:36:05 +0000

    fixed genes.tsv for genescorematrix

--- SummarizedExperimentMatrix2MM.R
+++ SummarizedExperimentMatrix2MM.R
@@ -10,7 +10,7 @@ if (type=='MotifMatrix'){
     quote=FALSE,sep="\t")
 } else if (type=='GeneScoreMatrix'){
     writeMM(MMatrix@assays@data@listData$GeneScoreMatrix,paste(prefix,"matrix.mtx",sep=""))
-    write(x = rownames(MMatrix@assays@data@listData$GeneScoreMatrix),file = paste(prefix,"genes.tsv",sep=""))
+    write(x = GSMatrix@elementMetadata@listData$name,file = paste(prefix,"genes.tsv",sep=""))
     write(x = colnames(MMatrix@assays@data@listData$GeneScoreMatrix),file = paste(prefix,"barcodes.tsv",sep=""))
     write.table(x = MMatrix@colData@listData, file = paste(prefix,"metadata.tsv",sep=""),
     quote=FALSE,sep="\t")
